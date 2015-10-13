a3e770ddf4b686a0fa46126e5413d664166ba7f1
Lance Laughlin <llaughlin@sapient.com>
2015-10-13 18:29:14 -0400

    Sass task setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,21 @@
-// include gulp
+// include modules
 var gulp = require('gulp'); 
-
-// include plug-ins
 var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
 
 // JS hint task
 gulp.task('jshint', function() {
   gulp.src('./src/scripts/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
+});
+
+gulp.task('sass', function () {
+  gulp.src('./src/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./build/css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./src/sass/**/*.scss', ['sass']);
 });
