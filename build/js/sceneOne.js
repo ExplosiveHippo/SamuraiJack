75d838713a20ff03b23b0ac422c99c8748ae79f3
Lance Laughlin <llaughlin@sapient.com>
2015-11-12 17:43:37 -0500

    Adding more scrollmagic

--- build/js/sceneOne.js
+++ build/js/sceneOne.js
@@ -1,10 +1,14 @@
-window.onload = function(){
+	var controller = new ScrollMagic.Controller();
     var castle = document.getElementById("castles");
-    var water = document.getElementById("water");
-    var treeLeft = document.getElementById("treeLeft");
-    var treeRight = document.getElementById("treeRight");
-    TweenLite.to(castle, 7, {bottom:"-170px", left:"100px", width: "1100px", ease:Expo.easeOut}).delay(1);
-    TweenLite.to(water, 5, {opacity:"0.5", bottom:"-250px", left:"300px", width: "900px", ease:Expo.easeOut}).delay(1);
-    TweenLite.to(treeLeft, 5, {left:"-100px", ease:Expo.easeOut}).delay(1);
-    TweenLite.to(treeRight, 5, {right:"-600px", width: "800px", ease:Expo.easeOut}).delay(1);
-}
+    // var water = document.getElementById("water");
+    // var treeLeft = document.getElementById("treeLeft");
+    // var treeRight = document.getElementById("treeRight");
+
+
+    var scene = new ScrollMagic.Scene({triggerElement: ".scene", duration: 300})
+    	.setTween("#castle",{bottom:"-170px", left:"100px", width: "1100px", ease:Expo.easeOut})
+    	.addTo(controller);
+    // TweenLite.to(castle, 7, ).delay(1);
+    // TweenLite.to(water, 5, {opacity:"0.5", bottom:"-250px", left:"300px", width: "900px", ease:Expo.easeOut}).delay(1);
+    // TweenLite.to(treeLeft, 5, {left:"-100px", ease:Expo.easeOut}).delay(1);
+    // TweenLite.to(treeRight, 5, {right:"-600px", width: "800px", ease:Expo.easeOut}).delay(1);
