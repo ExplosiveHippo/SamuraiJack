5c5ce3d2ff7b70ce0b08a9d310958fc357a0231a
Lance Laughlin <llaughlin@sapient.com>
2015-11-12 13:28:02 -0500

    Added scroll magic

--- build/js/sceneOne.js
+++ build/js/sceneOne.js
@@ -3,8 +3,8 @@ window.onload = function(){
     var water = document.getElementById("water");
     var treeLeft = document.getElementById("treeLeft");
     var treeRight = document.getElementById("treeRight");
-    TweenLite.to(castle, 7, {bottom:"-170px", left:"100px", width: "1100px", ease:Expo.easeOut}).delay(2);
-    TweenLite.to(water, 5, {opacity:"0.5", bottom:"-170px", left:"300px", width: "900px", ease:Expo.easeOut}).delay(2);
-    TweenLite.to(treeLeft, 5, {left:"-100px", ease:Expo.easeOut}).delay(2);
-    TweenLite.to(treeRight, 5, {right:"-600px", width: "800px", ease:Expo.easeOut}).delay(2);
+    TweenLite.to(castle, 7, {bottom:"-170px", left:"100px", width: "1100px", ease:Expo.easeOut}).delay(1);
+    TweenLite.to(water, 5, {opacity:"0.5", bottom:"-250px", left:"300px", width: "900px", ease:Expo.easeOut}).delay(1);
+    TweenLite.to(treeLeft, 5, {left:"-100px", ease:Expo.easeOut}).delay(1);
+    TweenLite.to(treeRight, 5, {right:"-600px", width: "800px", ease:Expo.easeOut}).delay(1);
 }
