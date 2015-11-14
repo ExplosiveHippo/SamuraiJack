fe0d082e2e3f4442bd759c406a85f348c1396380
Lance Laughlin <llaughlin@sapient.com>
2015-11-14 12:28:57 -0500

    Scene 1 done

--- build/js/sceneOne.js
+++ build/js/sceneOne.js
@@ -1,22 +1,26 @@
 	var controller = new ScrollMagic.Controller();
 
-    var scene1 = new ScrollMagic.Scene({triggerElement: ".scene", duration: 1200})
-    	.setTween("#castles",{bottom:"-800px", left:"-775px", width: "1800px", ease:Expo.easeOut})
-    	.addTo(controller);
+    var scene1 = new ScrollMagic.Scene({duration: 6000})
+        .setTween("#castles",{left:"-11250px", bottom: "-950px", scaleX:56, scaleY:56, ease:Expo.easeOut})
+        .addTo(controller);
 
-     var scene2 = new ScrollMagic.Scene({triggerElement: ".scene", duration: 300})
+    var scene2 = new ScrollMagic.Scene({duration: 300})
         .setTween("#water",{opacity:"0", bottom:"-150px", left:"300px", width: "900px", ease:Expo.easeOut})
         .addTo(controller);
 
-    var scene3 = new ScrollMagic.Scene({triggerElement: ".scene", duration: 1200})
+    var scene3 = new ScrollMagic.Scene({duration: 300})
         .setTween("#treeLeft",{left:"-550px", ease:Expo.easeOut})
         .addTo(controller);
 
-    var scene4 = new ScrollMagic.Scene({triggerElement: ".scene", duration: 1200})
+    var scene4 = new ScrollMagic.Scene({duration: 300})
         .setTween("#treeRight",{right:"-1200px", width: "800px", ease:Expo.easeOut})
         .addTo(controller);
 
-    var scene5 = new ScrollMagic.Scene({triggerElement: "#scene2", duration: 1200})
-        .setTween("#castle",{right:"-1800px", width: "800px", ease:Expo.easeOut})
-        .addTo(controller);
 
+    $(window).scroll(function (event) {
+        var scroll = $(window).scrollTop();
+        console.log(scroll);
+        if(scroll == 5215){
+            console.log("here");
+        }
+    });
